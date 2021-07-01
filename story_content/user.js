45136bc817b71a03e2b8791089abf8016f986afe
ANS <36783010+answebdev@users.noreply.github.com>
2021-07-01 15:59:55 -0700

    update certificate font

--- story_content/user.js
+++ story_content/user.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6EiuoGlyoo6":
+      case "6YDH58E5luI":
         Script1();
         break;
   }
@@ -16,23 +16,23 @@ function Script1()
     var yyyy = date.getFullYear();
     date = mm + '/' + dd + '/' + yyyy;
     var player = GetPlayer();
-    var name = player.GetVar("uName");
+    var name = player.GetVar("uName"); /* Replace uName with the Storyline variable holding the user's name */
     var doc = new jsPDF({
-      orientation: 'landscape'
+      orientation: 'landscape' /* Remove this line of code if your certificate is in portrait orientation */
     })
     var img = new Image;
     img.onload = function () {
       doc.addImage(this, 0, 0, 297, 210);
-      doc.setFontSize(40);
-      doc.setTextColor(99, 43, 43);
-      doc.setFont('Lato-Black', 'bold');
-      doc.text(name, (doc.internal.pageSize.width / 2), 120, null, null, 'center');
-      doc.setFont('Lato-Regular', 'normal');
+      doc.setFontSize(40); /* Set the font size by changing the number between the parentheses */
+      doc.setTextColor(99, 43, 43); /* Change the RGB text color by changing the numbers between the parentheses */
+      doc.setFont('Oswald-Bold', 'bold');
+      doc.text(name, (doc.internal.pageSize.width / 2), 120, null, null, 'center'); /* This tells the PDF to add text to the page. The first argument is the text to add (in this case a JavaScript variable), the second is the x-coordinate (in this case we’re using a function to find a middle point for the document), the third is the y-coordinate (in millimeters), the fourth and fifth arguments are null, and the sixth argument says that the text should be centered */
+      doc.setFont('Oswald-Regular', 'normal');
       doc.setFontSize(15);
-      doc.text(date, (doc.internal.pageSize.width / 2), 172, null, null, 'center');
-      doc.save("Certificate.pdf");
+      doc.text(date, (doc.internal.pageSize.width / 2), 172, null, null, 'center'); /* See above for the full explanation of the doc.text() function */
+      doc.save("Certificate.pdf"); /* Swap out ‘Certificate’ with what you want your certificate to be named */
     };
     img.crossOrigin = "";
-    img.src = "certificate.png";
+    img.src = "certificate.png"; /* Change the file name between these quotes to the file that you exported as the base certificate */
 }
 
